f3a2f3853db9912773db99c0701a745d917157b6
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2022-07-01 00:40:02 -0300

    Melhorando a abstração do Menu.

---
+++ LetsMarket.Menu/Funcionario.cs
@@ -0,0 +1,8 @@
+﻿namespace LetsMarket.Menu4
+{
+    public class Funcionarios
+    {
+        public static Action ListarFuncionarios = () => Console.WriteLine("Listando Funcionário");
+        public static Action CadastrarFuncionarios = () => Console.WriteLine("Cadastrando Funcionário");
+    }
+}
---
+++ LetsMarket.Menu/Produtos.cs
@@ -0,0 +1,8 @@
+﻿namespace LetsMarket.Menu4
+{
+    public class Produtos
+    {
+        public static Action ListarProdutos = () => Console.WriteLine("Listando Produtos");
+        public static Action CadastrarProdutos = () => Console.WriteLine("Cadastrando Produtos");
+    }
+}
