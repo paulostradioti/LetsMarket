15b30170b3bf34a0c15ff16eee11f37467a2a876
unknown <dudusegui@gmail.com>
2022-07-08 14:24:13 -0300

    Incluindo métodos nas classes Menu e Design. Alterações na classe Program.

--- LetsMarket/Program.cs
+++ LetsMarket/Program.cs
@@ -11,36 +11,39 @@ namespace LetsMarket
 
             VerificaLogin();
 
-            var menu = new MenuItem("Menu Principal");
 
-            var produtos = new MenuItem("Produtos");
-            produtos.Add(new MenuItem("Cadastrar Produtos", Produto.CadastrarProdutos));
-            produtos.Add(new MenuItem("Listar Produtos", Produto.ListarProdutos));
-            produtos.Add(new MenuItem("Editar Produtos", Produto.EditarProduto));
-            produtos.Add(new MenuItem("Remover Produtos", Produto.RemoverProduto));
+            Menu.CreateSubMenus();
 
-            var funcionarios = new MenuItem("Funcionários");
-            funcionarios.Add(new MenuItem("Cadastrar Funcionários", Funcionario.CadastrarFuncionarios));
-            funcionarios.Add(new MenuItem("Listar Funcionários", Funcionario.ListarFuncionarios));
-            funcionarios.Add(new MenuItem("Editar Funcionários", Funcionario.EditarFuncionarios));
-            funcionarios.Add(new MenuItem("Remover Funcionários", Funcionario.RemoverFuncionarios));
+            //var menu = new MenuItem("Menu Principal");
 
-            var clientes = new MenuItem("Clientes");
-            clientes.Add(new MenuItem("Cadastrar Clientes", Cliente.CadastrarClientes));
-            clientes.Add(new MenuItem("Listar Clientes", Cliente.ListarClientes));
-            clientes.Add(new MenuItem("Editar Clientes", Cliente.EditarClientes));
-            clientes.Add(new MenuItem("Remover Clientes", Cliente.RemoverClientes));
+            //var produtos = new MenuItem("Produtos");
+            //produtos.Add(new MenuItem("Cadastrar Produtos", Produto.CadastrarProdutos));
+            //produtos.Add(new MenuItem("Listar Produtos", Produto.ListarProdutos));
+            //produtos.Add(new MenuItem("Editar Produtos", Produto.EditarProduto));
+            //produtos.Add(new MenuItem("Remover Produtos", Produto.RemoverProduto));
 
-            var vendas = new MenuItem("Vendas");
-            vendas.Add(new MenuItem("Efetuar Venda", Vendas.EfetuarVenda));
+            //var funcionarios = new MenuItem("Funcionários");
+            //funcionarios.Add(new MenuItem("Cadastrar Funcionários", Funcionario.CadastrarFuncionarios));
+            //funcionarios.Add(new MenuItem("Listar Funcionários", Funcionario.ListarFuncionarios));
+            //funcionarios.Add(new MenuItem("Editar Funcionários", Funcionario.EditarFuncionarios));
+            //funcionarios.Add(new MenuItem("Remover Funcionários", Funcionario.RemoverFuncionarios));
 
-            menu.Add(produtos);
-            menu.Add(funcionarios);
-            menu.Add(clientes);
-            menu.Add(vendas);
-            menu.Add(new MenuItem("Sair", () => Environment.Exit(0)));
+            //var clientes = new MenuItem("Clientes");
+            //clientes.Add(new MenuItem("Cadastrar Clientes", Cliente.CadastrarClientes));
+            //clientes.Add(new MenuItem("Listar Clientes", Cliente.ListarClientes));
+            //clientes.Add(new MenuItem("Editar Clientes", Cliente.EditarClientes));
+            //clientes.Add(new MenuItem("Remover Clientes", Cliente.RemoverClientes));
 
-            menu.Execute();
+            //var vendas = new MenuItem("Vendas");
+            //vendas.Add(new MenuItem("Efetuar Venda", Vendas.EfetuarVenda));
+
+            //menu.Add(produtos);
+            //menu.Add(funcionarios);
+            //menu.Add(clientes);
+            //menu.Add(vendas);
+            //menu.Add(new MenuItem("Sair", () => Environment.Exit(0)));
+
+            //menu.Execute();
         }
 
         private static void ConfiguraPrompt()
--- LetsMarket/View/Menu.cs
+++ LetsMarket/View/Menu.cs
@@ -6,10 +6,47 @@ using System.Threading.Tasks;
 
 namespace LetsMarket
 {
-    internal class Menu
+    internal static class Menu
     {
         //aqui vale trazer as informações que estão no comecinho do Program.
         //Tem muita coisa lá que é só chamando menu do produto, menu do cliente, menu do funcionário
         //(e tentar otimizar para um único menu se possível)
+
+
+
+        public static void CreateSubMenus()
+        {
+            var menu = new MenuItem("Menu Principal");
+
+            var produtos = new MenuItem("Produtos");
+            produtos.Add(new MenuItem("Cadastrar Produtos", Produto.CadastrarProdutos));
+            produtos.Add(new MenuItem("Listar Produtos", Produto.ListarProdutos));
+            produtos.Add(new MenuItem("Editar Produtos", Produto.EditarProduto));
+            produtos.Add(new MenuItem("Remover Produtos", Produto.RemoverProduto));
+
+            var funcionarios = new MenuItem("Funcionários");
+            funcionarios.Add(new MenuItem("Cadastrar Funcionários", Funcionario.CadastrarFuncionarios));
+            funcionarios.Add(new MenuItem("Listar Funcionários", Funcionario.ListarFuncionarios));
+            funcionarios.Add(new MenuItem("Editar Funcionários", Funcionario.EditarFuncionarios));
+            funcionarios.Add(new MenuItem("Remover Funcionários", Funcionario.RemoverFuncionarios));
+
+            var clientes = new MenuItem("Clientes");
+            clientes.Add(new MenuItem("Cadastrar Clientes", Cliente.CadastrarClientes));
+            clientes.Add(new MenuItem("Listar Clientes", Cliente.ListarClientes));
+            clientes.Add(new MenuItem("Editar Clientes", Cliente.EditarClientes));
+            clientes.Add(new MenuItem("Remover Clientes", Cliente.RemoverClientes));
+
+            var vendas = new MenuItem("Vendas");
+            vendas.Add(new MenuItem("Efetuar Venda", Vendas.EfetuarVenda));
+
+            menu.Add(produtos);
+            menu.Add(funcionarios);
+            menu.Add(clientes);
+            menu.Add(vendas);
+            menu.Add(new MenuItem("Sair", () => Environment.Exit(0)));
+
+            menu.Execute();
+        }
+
     }
 }
